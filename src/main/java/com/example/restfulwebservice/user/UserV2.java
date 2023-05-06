5d5718c9a49137a4dbc09060319f1559bf944acc
=King <wodus331@gmail.com>
2023-05-06 20:35:33 +0900

    day2

--- src/main/java/com/example/restfulwebservice/user/UserV2.java
+++ src/main/java/com/example/restfulwebservice/user/UserV2.java
@@ -3,6 +3,8 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -10,15 +12,9 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
-@JsonFilter("UserInfo")
-public class UserV2 {
-    private Integer id;
+@NoArgsConstructor
+@JsonFilter("UserInfoV2")
+public class UserV2 extends User {
 
-    @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
-    private String name;
-    @Past(message = "미래 날짜는 설절될 수 없습니다.")
-    private Date joinDate;
-
-    private String password;
-    private String ssn;
+   private String grade;
 }
